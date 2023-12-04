6af110fccdff6cd7b4cad6cffe38fbf441dab3d6
Fynn <fygerh@gmail.com>
2023-12-04 22:02:20 +0100

    NREL 5MW geometry added. Error occurs for 1DSpline

--- single_turbine_simulation/start_simulation.jl
+++ single_turbine_simulation/start_simulation.jl
@@ -24,7 +24,7 @@ paraview        = true                      # Whether to visualize with Paraview
 # ----------------- GEOMETRY PARAMETERS ----------------------------------------
 
 # Rotor geometry
-rotor_file      = "apc10x7.csv"                                             # Rotor geometry
+rotor_file      = "NREL5MW.csv"                                             # Rotor geometry #apc10x7.csv
 data_path       = joinpath(start_simulation_path, "..", "database")         # Path to rotor database
 pitch           = 0.0                                                       # (deg) collective pitch of blades
 CW              = false                                                     # Clock-wise rotation
@@ -53,8 +53,8 @@ R, B            = uns.read_rotor(rotor_file; data_path=data_path)[[1,3]]
 # ----------------- SIMULATION PARAMETERS --------------------------------------
 
 # Operating conditions
-RPM             = 9200                      # RPM
-J               = 0.4                       # Advance ratio Vinf/(nD)
+RPM             = 12.1                      # RPM
+J               = 11.4/((RPM/60)*2*R)      # Advance ratio Vinf/(nD)
 AOA             = 0                         # (deg) Angle of attack (incidence angle)
 
 rho             = 1.225                     # (kg/m^3) air density
@@ -118,4 +118,144 @@ if VehicleType == uns.QVLMVehicle
     #       quasi-steady solver will probe induced velocities at the lifting
     #       line of the blade
     uns.vlm.VLMSolver._mute_warning(true)
+end
+
+# ----------------- 1) VEHICLE DEFINITION --------------------------------------
+println("Generating geometry...")
+
+# Generate rotor
+rotor = uns.generate_rotor(rotor_file; pitch=pitch,
+                                        n=n, CW=CW, blade_r=r,
+                                        altReD=[RPM, J, mu/rho],
+                                        xfoil=xfoil,
+                                        ncrit=ncrit,
+                                        data_path=data_path,
+                                        verbose=true,
+                                        verbose_xfoil=false,
+                                        plot_disc=true
+                                        );
+
+println("Generating vehicle...")
+
+# Generate vehicle
+system = vlm.WingSystem()                   # System of all FLOWVLM objects
+vlm.addwing(system, "Rotor", rotor)
+
+rotors = [rotor];                           # Defining this rotor as its own system
+rotor_systems = (rotors, );                 # All systems of rotors
+
+wake_system = vlm.WingSystem()              # System that will shed a VPM wake
+                                            # NOTE: Do NOT include rotor when using the quasi-steady solver
+if VehicleType != uns.QVLMVehicle
+    vlm.addwing(wake_system, "Rotor", rotor)
+end
+
+vehicle = VehicleType(   system;
+                            rotor_systems=rotor_systems,
+                            wake_system=wake_system
+                         );
+
+# NOTE: Through the `rotor_systems` keyword argument to `uns.VLMVehicle` we
+#       have declared any systems (groups) of rotors that share a common RPM.
+#       We will later declare the control inputs to each rotor system when we
+#       define the `uns.KinematicManeuver`.
+
+
+# ------------- 2) MANEUVER DEFINITION -----------------------------------------
+# Non-dimensional translational velocity of vehicle over time
+Vvehicle(t) = zeros(3)
+
+# Angle of the vehicle over time
+anglevehicle(t) = zeros(3)
+
+# RPM control input over time (RPM over `RPMref`)
+RPMcontrol(t) = 1.0
+
+angles = ()                                 # Angle of each tilting system (none)
+RPMs = (RPMcontrol, )                       # RPM of each rotor system
+
+maneuver = uns.KinematicManeuver(angles, RPMs, Vvehicle, anglevehicle)
+
+# NOTE: `FLOWUnsteady.KinematicManeuver` defines a maneuver with prescribed
+#       kinematics. `Vvehicle` defines the velocity of the vehicle (a vector)
+#       over time. `anglevehicle` defines the attitude of the vehicle over time.
+#       `angle` defines the tilting angle of each tilting system over time.
+#       `RPM` defines the RPM of each rotor system over time.
+#       Each of these functions receives a nondimensional time `t`, which is the
+#       simulation time normalized by the total time `ttot`, from 0 to
+#       1, beginning to end of simulation. They all return a nondimensional
+#       output that is then scaled by either a reference velocity (`Vref`) or
+#       a reference RPM (`RPMref`). Defining the kinematics and controls of the
+#       maneuver in this way allows the user to have more control over how fast
+#       to perform the maneuver, since the total time, reference velocity and
+#       RPM are then defined in the simulation parameters shown below.
+
+
+# ------------- 3) SIMULATION DEFINITION ---------------------------------------
+
+Vref = 0.0                                  # Reference velocity to scale maneuver by
+RPMref = RPM                                # Reference RPM to scale maneuver by
+
+Vinit = Vref*Vvehicle(0)                    # Initial vehicle velocity
+Winit = pi/180*(anglevehicle(1e-6) - anglevehicle(0))/(1e-6*ttot)  # Initial angular velocity
+
+simulation = uns.Simulation(vehicle, maneuver, Vref, RPMref, ttot;
+                                                    Vinit=Vinit, Winit=Winit);
+
+
+# ------------- 4) MONITORS DEFINITIONS ----------------------------------------
+figs, figaxs = [], []                       # Figures generated by monitor
+
+# Generate rotor monitor
+monitor_rotor = uns.generate_monitor_rotors(rotors, J, rho, RPM, nsteps;
+                                            t_scale=RPM/60,        # Scaling factor for time in plots
+                                            t_lbl="Revolutions",   # Label for time axis
+                                            out_figs=figs,
+                                            out_figaxs=figaxs,
+                                            save_path=save_path,
+                                            run_name=run_name,
+                                            figname="rotor monitor",
+                                            )
+
+
+# ------------- 5) RUN SIMULATION ----------------------------------------------
+println("Running simulation...")
+
+uns.run_simulation(simulation, nsteps;
+                    # ----- SIMULATION OPTIONS -------------
+                    Vinf=Vinf,
+                    rho=rho, mu=mu, sound_spd=speedofsound,
+                    # ----- SOLVERS OPTIONS ----------------
+                    p_per_step=p_per_step,
+                    max_particles=max_particles,
+                    vpm_viscous=vpm_viscous,
+                    sigma_vlm_surf=sigma_rotor_surf,
+                    sigma_rotor_surf=sigma_rotor_surf,
+                    sigma_vpm_overwrite=sigma_vpm_overwrite,
+                    vlm_rlx=vlm_rlx,
+                    hubtiploss_correction=hubtiploss_correction,
+                    shed_unsteady=shed_unsteady,
+                    shed_starting=shed_starting,
+                    extra_runtime_function=monitor_rotor,
+                    # ----- OUTPUT OPTIONS ------------------
+                    save_path=save_path,
+                    run_name=run_name,
+                    );
+
+
+# ----------------- 6) VISUALIZATION -------------------------------------------
+if paraview
+    println("Calling Paraview...")
+
+    # Files to open in Paraview
+    files = joinpath(save_path, run_name*"_pfield...xmf;")
+    for bi in 1:B
+        global files
+        files *= run_name*"_Rotor_Blade$(bi)_loft...vtk;"
+        files *= run_name*"_Rotor_Blade$(bi)_vlm...vtk;"
+    end
+
+    # Call Paraview
+    run(`paraview --data=$(files)`)
+
 end
